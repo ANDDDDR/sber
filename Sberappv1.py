b3e29aeef20bc3d9ab6f8ef010c26beb2a1aead4
ANDDDDR <48557683+ANDDDDR@users.noreply.github.com>
2019-07-13 16:28:02 +0300

    Update Sberappv1.py

--- Sberappv1.py
+++ Sberappv1.py
@@ -1,11 +1,35 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+Examinations = [
+    {        
+	
+        'subject': 'OS',
+	'exam grade':'excellent'
+    },
+    {
+        
+        'subject': 'Computational Mathematics',
+	'exam grade':'good'
+    },
+	{        
+	
+        'subject': 'electrical engineering',
+	'exam grade':'good'
+    },
+    {
+        
+        'subject': 'computer and peripherals',
+	'exam grade':'excellent'
+    }
+]
+
+@app.route('/VIp21/Muravev_Andrey_Vladimirovich', methods=['GET'])
+def get_Examinations():
+    return jsonify({'Examinations': Examinations})
 
 if __name__ == '__main__':
     app.run(debug=True)
